69f6b000dae3e8cc5ebe2a94702b30d1fe54f327
Ricky Leong <rickyhhleong@gmail.com>
2024-04-16 23:21:26 +1200

    Including vault configuration code (to be amended).

---
+++ TenkiAme/ServiceConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using TenkiAme.Models;
+
+namespace TenkiAme
+{
+    public static class ServiceConfiguration
+    {
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            var vaultName = "TenkiAme-Secrets";
+            var keyVaultUrl = $"https://{vaultName}.vault.azure.net";
+            var client = new SecretClient(vaultUri: new Uri(keyVaultUrl), credential: new DefaultAzureCredential());
+
+            var secretsDict = new string[] { "MetOcean-Key" }
+                .Select(async name => await client.GetSecretAsync(name))
+                .Select(task => task.Result.Value)
+                .ToDictionary(secret => secret.Name, secret => secret.Value);
+
+            foreach (var name in new string[] { "MetOcean-Key" })
+            {
+                Console.Out.WriteLine($"{name}: {secretsDict[name]}");
+            }
+
+            services.AddSingleton<IDictionary<string, string>>(provider => secretsDict);
+            //services.AddSingleton<WeatherAPIService>();
+        }
+    }
+}
