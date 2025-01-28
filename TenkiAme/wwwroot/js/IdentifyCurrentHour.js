dd30baa49eed0f1a58e896a470542b71e80cccbe
Ricky Leong <rickyhhleong@gmail.com>
2025-01-28 23:35:21 +1300

    Updated js GetCurrentNZTimeAsDate() function to do in place conversion

--- TenkiAme/wwwroot/js/IdentifyCurrentHour.js
+++ TenkiAme/wwwroot/js/IdentifyCurrentHour.js
@@ -11,31 +11,28 @@ function GetCurrentNZTimeAsDate() {
     var splitDate = currentDate.split(", ");
 
     //Split the components into lists of the individual values
-    var dateComponentStr = splitDate[0].split("/");
-    var timeComponentStr = splitDate[1].replaceAll(":", ' ').split(' ');
+    var dateComponent = splitDate[0].split("/");
+    var timeComponent = splitDate[1].replaceAll(":", ' ').split(' ');
 
-    //Now convert the components into lists of integers
-    var dateComponentInt = [];
-    var timeComponentInt = []; 
-
+    //Now convert the components into lists of integers; 
     for (var i = 0; i < 3; i++) {
-        dateComponentInt.push(parseInt(dateComponentStr[i]));
+        dateComponent[i] = parseInt(dateComponent[i]);
     }
 
     for (var i = 0; i < 3; i++) {
-        timeComponentInt.push(parseInt(timeComponentStr[i]));
+        timeComponent[i] = parseInt(timeComponent[i]);
     }
 
     //If it's 1pm onwards then add 12 hours to the time component
-    timeComponentInt[0] = timeComponentInt[0] < 12 && timeComponentStr[3] === 'pm' ? timeComponentInt[0] + 12 : timeComponentInt[0];
+    timeComponent[0] = timeComponent[0] < 12 && timeComponent[3] === 'pm' ? timeComponent[0] + 12 : timeComponent[0];
 
     return new Date(
-        dateComponentInt[2], //Year
-        dateComponentInt[1] - 1, //Month (goes from 0 to 11)
-        dateComponentInt[0], //Day
-        timeComponentInt[0], //Hours
-        timeComponentInt[1], //Minutes
-        timeComponentInt[2]  //Seconds
+        dateComponent[2], //Year
+        dateComponent[1] - 1, //Month (goes from 0 to 11)
+        dateComponent[0], //Day
+        timeComponent[0], //Hours
+        timeComponent[1], //Minutes
+        timeComponent[2]  //Seconds
     );
 }
 
